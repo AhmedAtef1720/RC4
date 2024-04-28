f940463abdfd09d2ec5772ca8022ec53d0e38389
AhmedAtef1720 <168336170+AhmedAtef1720@users.noreply.github.com>
2024-04-28 13:55:57 +0300

    Update main.dart

--- example/main.dart
+++ example/main.dart
@@ -4,7 +4,7 @@ import 'package:simple_rc4/simple_rc4.dart';
 
 void main() {
   var rc4 = RC4('zonble');
-  var input = '中文';
+  var input = 'Ahmed';
   var bytes = rc4.encodeBytes(utf8.encode(input));
   rc4 = RC4('zonble');
   var output = rc4.decodeBytes(bytes);
